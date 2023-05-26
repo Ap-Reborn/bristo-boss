a297cd2c020b90921ee74473d3245a9058d093ab
ashraful amin <ashrafulamin654@gmail.com>
2023-05-27 03:45:28 +0600

    75-3 Create Custom Hook Usemenu To Load Menu Data

--- src/Pages/Home/Menu/Menu.jsx
+++ src/Pages/Home/Menu/Menu.jsx
@@ -1,7 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg';
-import PopularMenu from '../PopularMenu/PopularMenu';
 const Menu = () => {
     return (
         <div>
@@ -10,14 +9,7 @@ const Menu = () => {
             </Helmet>
             <Cover img={menuImg}
                 title='our menu'></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg}
-                title='our menu'></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg}
-                title='our menu'></Cover>
-            <PopularMenu></PopularMenu>
-        </div>
+  </div>
     );
 };
 
--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -3,18 +3,21 @@
 import { useEffect, useState } from "react";
 import SectionTitle from "../../../Component/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-    const [menu,setMenu]=useState([]);
-    useEffect(()=>{
-        fetch('menu.json')
-        .then(res=>res.json())
-        .then(data =>{
-            const popularItems =data.filter(item=> item.category === 'popular');
-            setMenu(popularItems)
-        })
-    },[])
-    console.log(menu)
+    const [menu]=useMenu();
+    const popular = menu.filter(item => item.category === 'popular')
+    // const [menu,setMenu]=useState([]);
+    // useEffect(()=>{
+    //     fetch('menu.json')
+    //     .then(res=>res.json())
+    //     .then(data =>{
+    //         const popularItems =data.filter(item=> item.category === 'popular');
+    //         setMenu(popularItems)
+    //     })
+    // },[])
+    // console.log(menu)
     return (
         <div>
    <section className="mb-12">
@@ -25,7 +28,7 @@ const PopularMenu = () => {
     </SectionTitle>
     <div className="grid md:grid-cols-2 gap-10">
         {
-            menu.map(item=><MenuItem
+            popular.map(item=><MenuItem
             key={item._id}
             item={item}
             ></MenuItem>)
