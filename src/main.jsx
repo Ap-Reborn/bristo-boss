fc733173e8b1658efa9d997c7adbcf363da352d1
ashraful amin <ashrafulamin654@gmail.com>
2023-05-30 22:09:37 +0600

    76-3 Firebase Project Setup For Bistro Boss

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,17 @@ import {
 } from "react-router-dom";
 import { router } from './Route/Router.jsx';
 import {HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './Provider/AuthProvider';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
+ <AuthProvider>
+ <HelmetProvider>
       <div className='max-w-screen-xl mx-auto'>
         <RouterProvider router={router} />
       </div>
     </HelmetProvider>
+ </AuthProvider>
   </React.StrictMode>,
 )
