ad6b34562721b4169a678539b2fc0f7b9320981e
ashraful amin <ashrafulamin654@gmail.com>
2023-06-04 11:25:46 +0600

    77-3 Save Cart data to the server

--- src/Component/FoodCard/FoodCard.jsx
+++ src/Component/FoodCard/FoodCard.jsx
@@ -3,23 +3,31 @@
 import { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
 import Swal from "sweetalert2";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const FoodCard = ({ item }) => {
-  const { name, image, price, recipe } = item;
+  const { name, image, price, recipe,_id } = item;
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
+  const location = useLocation();
   const handleAddToCart = (item) => {
     console.log(item)
-    if (user) {
-      fetch('http://localhost:5000/carts')
+    if (user && user.email) {
+      const cartItem={menuItemId: _id,name,price,email: user.email}
+      fetch('http://localhost:5000/carts',{
+        method:'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body:JSON.stringify(cartItem)
+      })
         .then(res => res.json())
         .then(data => {
           if (data.insertedId) {
             Swal.fire({
               position: 'top-end',
               icon: 'success',
-              title: 'Your work has been saved',
+              title: 'food added on the cart',
               showConfirmButton: false,
               timer: 1500
             })
@@ -36,26 +44,26 @@ const FoodCard = ({ item }) => {
         confirmButtonText: 'login now'
       }).then((result) => {
         if (result.isConfirmed) {
-          navigate('/login')
-        }
-      })
+          navigate('/login', {state : { from: location }})
     }
+  })
+}
   }
-  return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <figure><img src={image} alt="photo" /></figure>
-      <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text white">
-        $ {price}
-      </p>
-      <div className="card-body flex flex-col items-center">
-        <h2 className="card-title">{name}</h2>
-        <p>{recipe}</p>
-        <div className="card-actions justify-end">
-          <button onClick={() => handleAddToCart(item)} className="btn btn-outline border-0 border-b-4 mt-4 bg-slate-100 border-orange-400">Add to Cart</button>
-        </div>
+return (
+  <div className="card w-96 bg-base-100 shadow-xl">
+    <figure><img src={image} alt="photo" /></figure>
+    <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text white">
+      $ {price}
+    </p>
+    <div className="card-body flex flex-col items-center">
+      <h2 className="card-title">{name}</h2>
+      <p>{recipe}</p>
+      <div className="card-actions justify-end">
+        <button onClick={() => handleAddToCart(item)} className="btn btn-outline border-0 border-b-4 mt-4 bg-slate-100 border-orange-400">Add to Cart</button>
       </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default FoodCard;
