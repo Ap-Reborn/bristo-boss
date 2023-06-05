3c64466807b530aa6fcfa989e01449baea4fc261
ashraful amin <ashrafulamin654@gmail.com>
2023-06-05 16:10:27 +0600

    78-5 Display All users and Create Make admin API

--- src/Pages/DashBoard/MyCard/AllUsers/AllUsers.jsx
+++ src/Pages/DashBoard/MyCard/AllUsers/AllUsers.jsx
@@ -1,16 +1,63 @@
 // import React from 'react';
 
 import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
+import { FaTrashAlt, FaUserShield } from "react-icons/fa";
 
 const AllUsers = () => {
     const { data: users = [], refetch } = useQuery(['users'], async () => {
         const res = await fetch('http://localhost:5000/users');
         return res.json();
     });
+const handleMakeAdmin =()=>{
 
+}
+const handleDelete=()=>{
+    
+}
     return (
-        <div>
-            {users.length}
+        <div className="w-full">
+            <Helmet>
+                <title>Bristo / allUsers</title>
+            </Helmet>
+            <h3 className="text-3xl font-semibold my-4">total users: {users.length}</h3>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) =>
+                                <tr
+                                    key={user._id}
+                                >
+                                    <th>{index + 1}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{
+                                        user.role === 'admin'? 'admin':
+                                        <button onClick={() => handleMakeAdmin(user._id)} className="btn btn-ghost bg-orange-600 text-white"><FaTrashAlt></FaTrashAlt>
+                                        <FaUserShield></FaUserShield>
+                                        </button>
+                                        }</td>
+                                    <td>
+                                        <button onClick={() => handleDelete(user)} className="btn btn-ghost bg-red-600 text-white"><FaTrashAlt></FaTrashAlt></button>
+                                    </td>
+                                </tr>
+                            )
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
