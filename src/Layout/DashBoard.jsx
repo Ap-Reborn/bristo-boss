796055d1c13ed3d5b0280f0e56b91de0d47f53e2
ashraful amin <ashrafulamin654@gmail.com>
2023-06-05 15:34:54 +0600

    78-4 Load all users on the Dashboard page

--- src/Layout/DashBoard.jsx
+++ src/Layout/DashBoard.jsx
@@ -1,12 +1,14 @@
 // import React from 'react';
 
 import { NavLink, Outlet } from "react-router-dom";
-import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome } from 'react-icons/fa';
+import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome, FaUtensils, FaUser, FaBook } from 'react-icons/fa';
 import useCart from "../hooks/useCart";
 
 
 const DashBoard = () => {
   const [cart] = useCart();
+  // todo : load data from the server to have dynamic isAdmin based on data
+  const isAdmin = true;
   return (
     <div className="drawer drawer-mobile">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -19,18 +21,33 @@ const DashBoard = () => {
       <div className="drawer-side bg-[#D1A054]">
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
         <ul className="menu p-4 w-80 h-full">
-          {/* Sidebar content here */}
-          <li><NavLink to='/dashboard/home'><FaHome></FaHome> user home</NavLink></li>
-          <li><NavLink to='/dashboard/reservation'><FaCalendarAlt></FaCalendarAlt> reservation</NavLink></li>
-          <li><NavLink to='/dashboard/history'><FaWallet></FaWallet> payment history</NavLink></li>
-          <li>
-            <NavLink to='/dashboard/mycard'><FaShoppingCart></FaShoppingCart> my card
-              <span>
 
-                <div className="badge badge-secondary">+{cart?.length || 0}</div>
-              </span>
-            </NavLink>
-          </li>
+          {
+            isAdmin ? <>
+
+              <li><NavLink to='/dashboard/home'><FaHome></FaHome> Admin Home</NavLink></li>
+              <li><NavLink to='/dashboard/additem'><FaUtensils></FaUtensils> Add Items</NavLink></li>
+              <li><NavLink to='/dashboard/manageitem'><FaWallet></FaWallet> Manage Item</NavLink></li>
+              <li><NavLink to='/dashboard/bookings'><FaBook></FaBook> Manage Bookings</NavLink></li>
+              <li><NavLink to='/dashboard/allusers'><FaUser></FaUser>All Users</NavLink></li>
+            </>
+              :
+              <>
+
+                {/* Sidebar content here */}
+                <li><NavLink to='/dashboard/home'><FaHome></FaHome> user home</NavLink></li>
+                <li><NavLink to='/dashboard/reservation'><FaCalendarAlt></FaCalendarAlt> reservation</NavLink></li>
+                <li><NavLink to='/dashboard/history'><FaWallet></FaWallet> payment history</NavLink></li>
+                <li>
+                  <NavLink to='/dashboard/mycard'><FaShoppingCart></FaShoppingCart> my card
+                    <span>
+
+                      <div className="badge badge-secondary">+{cart?.length || 0}</div>
+                    </span>
+                  </NavLink>
+                </li>
+              </>
+          }
           <div className="divider"></div>
           <li><NavLink to='/'><FaHome></FaHome> home</NavLink></li>
           <NavLink to='/menu'>our menu</NavLink>
--- src/Route/Router.jsx
+++ src/Route/Router.jsx
@@ -11,6 +11,7 @@ import Secret from "../Pages/Shared/Secret/Secret";
 import PrivateRoute from "./PrivateRoute";
 import DashBoard from "../Layout/DashBoard";
 import MyCard from "../Pages/DashBoard/MyCard/MyCard";
+import AllUsers from "../Pages/DashBoard/MyCard/AllUsers/AllUsers";
 
   export const router = createBrowserRouter([
     {
@@ -50,6 +51,10 @@ import MyCard from "../Pages/DashBoard/MyCard/MyCard";
         {
           path:'mycard',
           element:<MyCard></MyCard>
+        },
+        {
+          path:'allusers',
+          element:<AllUsers></AllUsers>
         }
       ]
     }
