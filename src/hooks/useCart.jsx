a723509ed5ecee5b0b459e00fe14d33bb7447dc0
ashraful amin <ashrafulamin654@gmail.com>
2023-06-06 20:19:04 +0600

    78-9 (optional)(advanced) axios interceptor using chat gpt

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -2,25 +2,44 @@
 import { useQuery } from '@tanstack/react-query'
 import { useContext } from 'react';
 import { AuthContext } from '../Provider/AuthProvider';
+import useAxiosSecure from './useAxiouSecure';
 
 
 const useCart = () => {
     const { user } = useContext(AuthContext);
     // 78.8 start
     const token = localStorage.getItem('access-token');
-    // nica fetch er modde header and authorization kora hoica
+   const [axiosSecure] = useAxiosSecure();
+     // nica fetch er modde header and authorization kora hoica
     // 78.8 end
-    const { refetch,data: cart = [] } = useQuery({
+    //78.9 start
+    // nica await er pse axioussecure bosalam fetch kete
+    // http://localhost:5000 kete dea hoica
+    /*  ,{
+        headers:{
+            authorization:`bearer ${token}`
+        }} fele dea hoica */
+    //78.9 end
+    const { refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
+        // queryFn: async () => {
+        //     const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {
+        //         headers: {
+        //             authorization: `bearer ${token}`
+        //         }
+        //     })
+        //     return res.json();
+        // },
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`,{
-                headers:{
-                authorization:`bearer ${token}`
-            }})
-            return res.json();
+            const res = await axiosSecure(`/carts?email=${user?.email}`)
+           console.log('res from axios',res)
+            return res.data;
         },
+
+
+     
     })
-    return [cart,refetch]
+    return [cart, refetch]
 };
 
 export default useCart;
