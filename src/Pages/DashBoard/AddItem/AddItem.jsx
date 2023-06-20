d09bf61d50ddaec642b1298ed8db76c1c213a8ba
ashraful amin <ashrafulamin654@gmail.com>
2023-06-20 13:10:36 +0600

    79-6 Get image url and create save new item api

--- src/Pages/DashBoard/AddItem/AddItem.jsx
+++ src/Pages/DashBoard/AddItem/AddItem.jsx
@@ -2,23 +2,46 @@
 
 import SectionTitle from "../../../Component/SectionTitle/SectionTitle";
 import { useForm } from 'react-hook-form';
-const img_hosting_token=import.meta.env.VITE_Image_Upload_token;
+import useAxiosSecure from "../../../hooks/useAxiouSecure";
+import Swal from "sweetalert2";
+const img_hosting_token = import.meta.env.VITE_Image_Upload_token;
 const AddItem = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const img_hosting_url=`https://api.imgbb.com/1/upload?expiration=600&key=YOUR_CLIENT_API_KEY=${img_hosting_token}`
+    const [axiosSecure] = useAxiosSecure();
+    const { register, handleSubmit,reset, formState: { errors } } = useForm();
+    const img_hosting_url = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`
     const onSubmit = data => {
         // console.log(data)
-        const formData=new FormData();
-        formData.append('image',data.image[0])
+        const formData = new FormData();
+        formData.append('image', data.image[0])
 
         fetch(img_hosting_url, {
             method: 'POST',
             body: formData
         })
-        .then(res => res.json())
-        .then(imgResponse => {
-            console.log(imgResponse)
-        })
+            .then(res => res.json())
+            .then(imgResponse => {
+                if (imgResponse.success) {
+                    const imgURL = imgResponse.data.display_url;
+                    console.log(data,imgURL)
+                    const { name, price, category, recipe } = data;
+                    const newItem = { name, price: parseFloat(price), category, recipe, image: imgURL }
+                    console.log(newItem)
+                    axiosSecure.post('/menu', newItem)
+                        .then(data => {
+                            console.log('after posting new menu item', data.data)
+                            if (data.data.insertedId) {
+                                reset();
+                                Swal.fire({
+                                    position: 'top-end',
+                                    icon: 'success',
+                                    title: 'Item added successfully',
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                })
+                            }
+                        })
+                }
+            })
     };
     console.log(errors);
     console.log(img_hosting_token);
@@ -30,33 +53,33 @@ const AddItem = () => {
                     <label className="label">
                         <span className="label-text font-semibold">Recipe Name </span>
                     </label>
-                    <input type="text" placeholder="Recipe Name" {...register("name", {required: true, maxLength: 120})} className="input input-bordered w-full " />
+                    <input type="text" placeholder="Recipe Name" {...register("name", { required: true, maxLength: 120 })} className="input input-bordered w-full " />
 
                 </div>
 
 
-             <div className="flex my-4">
-             <div className="form-control w-full ">
-                    <label className="label">
-                        <span className="label-text">Category*</span>
-                    </label>
-                    <select defaultValue='pick One' {...register("category", { required: true })} className="select select-bordered">
-                        <option disabled>Pick one</option>
-                        <option>Pizza</option>
-                        <option>Soup</option>
-                        <option>Salad</option>
-                        <option>Dessert</option>
-                        <option>Desi</option>
-                        <option>Drinks</option>
-                    </select>
+                <div className="flex my-4">
+                    <div className="form-control w-full ">
+                        <label className="label">
+                            <span className="label-text">Category*</span>
+                        </label>
+                        <select defaultValue='pick One' {...register("category", { required: true })} className="select select-bordered">
+                            <option disabled>Pick one</option>
+                            <option>Pizza</option>
+                            <option>Soup</option>
+                            <option>Salad</option>
+                            <option>Dessert</option>
+                            <option>Desi</option>
+                            <option>Drinks</option>
+                        </select>
+                    </div>
+                    <div className="form-control w-full ml-4">
+                        <label className="label">
+                            <span className="label-text font-semibold">Price*</span>
+                        </label>
+                        <input type="number"{...register("price", { required: true })} placeholder="Type here" className="input input-bordered w-full " />
+                    </div>
                 </div>
-                <div className="form-control w-full ml-4">
-                    <label className="label">
-                        <span className="label-text font-semibold">Price*</span>
-                    </label>
-                    <input type="number"{...register("price", { required: true })} placeholder="Type here" className="input input-bordered w-full " />
-                </div>
-             </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Recipe Details</span>
