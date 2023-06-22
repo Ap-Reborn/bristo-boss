38a8b8f269e215ee9df330193ddfe263724dd687
ashraful amin <ashrafulamin654@gmail.com>
2023-06-22 12:01:02 +0600

    80-3 Create Stripe account and use publishable key

--- src/Pages/DashBoard/Payment/Payment.jsx
+++ src/Pages/DashBoard/Payment/Payment.jsx
@@ -1,12 +1,20 @@
 // import React from 'react';
 
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../Component/SectionTitle/SectionTitle";
-
+import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
+// todo provide publis key
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 const Payment = () => {
     return (
         <div>
              <SectionTitle subHeading="please process" heading="Payment"></SectionTitle>
             <h2>money tranfer gate wayyyyyyyyyy</h2>
+       <Elements stripe={stripePromise}>
+
+        <CheckoutForm></CheckoutForm>
+       </Elements>
         </div>
     );
 };
