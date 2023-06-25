017bbc65c363612629d1289eb1f441bd67fcc0d9
ashraful amin <ashrafulamin654@gmail.com>
2023-06-25 20:43:34 +0600

    80-6 Get Price for payment intent and understand array reduce

--- src/Pages/DashBoard/Payment/Payment.jsx
+++ src/Pages/DashBoard/Payment/Payment.jsx
@@ -4,16 +4,20 @@ import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../Component/SectionTitle/SectionTitle";
 import CheckoutForm from "./CheckoutForm";
 import { Elements } from "@stripe/react-stripe-js";
+import useCart from "../../../hooks/useCart";
 // todo provide publis key
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 const Payment = () => {
+    const [cart] = useCart();
+    const total = cart.reduce((sum, item) => sum + item.price, 0);
+    const price = parseFloat(total.toFixed(2))
     return (
         <div>
              <SectionTitle subHeading="please process" heading="Payment"></SectionTitle>
             <h2>money tranfer gate wayyyyyyyyyy</h2>
        <Elements stripe={stripePromise}>
 
-        <CheckoutForm></CheckoutForm>
+        <CheckoutForm price={price}></CheckoutForm>
        </Elements>
         </div>
     );
